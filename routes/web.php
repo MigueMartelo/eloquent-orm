836c901aeca88d2a2f395730a02236f8e89baba9
vagrant <vagrant@homestead>
2017-05-24 20:20:34 +0000

    Eloquent crear y actualizar

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use EloquentORM\User;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/create', function () {
+    
+	$user = User::create([
+		'name' =>  'Ana Fernandez',
+		'email' => 'ana@gmail.com',
+		'password' => bcrypt('123456'),
+		'gender' => 'f',
+		'biography' => 'Administradora financiera'
+	]);
+
+	return 'Usuario Guardado';
+
+});
+
+Route::get('/update', function () {
+    
+	$user = User::find(1);
+
+	$user->gender = 'm';
+	$user->biography = 'Líder tecnológico UTB';
+	$user->save();
+
+	return 'Usuario Actualizado';
+
 });
