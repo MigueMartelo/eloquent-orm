89d7b99398eadb2734a902d2eb7a9dd85e30728b
vagrant <vagrant@homestead>
2017-05-24 20:42:30 +0000

    Eloquent CRUD

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,43 @@
 |
 */
 use EloquentORM\User;
+use Faker\Factory as Faker;
 
 Route::get('/create', function () {
+
+	$faker = Faker::create();
     
 	$user = User::create([
-		'name' =>  'Ana Fernandez',
-		'email' => 'ana@gmail.com',
+		'name' =>  $faker->name,
+		'email' => $faker->email,
 		'password' => bcrypt('123456'),
-		'gender' => 'f',
-		'biography' => 'Administradora financiera'
+		'gender' => $faker->randomElement(['f', 'm']),
+		'biography' => $faker->text(255)
 	]);
 
-	return 'Usuario Guardado';
-
+	return $user;
 });
 
-Route::get('/update', function () {
-    
-	$user = User::find(1);
+Route::get('/read/{id}', function ($id){
+	$user = User::find($id);
+	return $user;
+});
 
-	$user->gender = 'm';
-	$user->biography = 'Líder tecnológico UTB';
+Route::get('/update/{id}', function ($id) {
+    $faker = Faker::create();
+	$user = User::find($id);
+
+	$user->name = $faker->name;
+	$user->gender = $faker->randomElement(['f', 'm']);
+	$user->biography = $faker->text(255);
 	$user->save();
 
-	return 'Usuario Actualizado';
+	return $user;
+});
 
+Route::get('/delete/{id}', function ($id){
+	$user = User::find($id);
+	$user->delete();
+
+	return 'Usuario Eliminado';
 });
