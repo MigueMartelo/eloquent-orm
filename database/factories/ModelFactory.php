9af5b6b921ecc53e2b5509fcb7d35ebac063d08f
vagrant <vagrant@homestead>
2017-05-24 21:46:01 +0000

    User Seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,6 +19,8 @@ $factory->define(EloquentORM\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'gender' => $faker->randomElement(['f', 'm']),
+        'biography' => $faker->text(255),
+        'remember_token' => $faker->randomElement([null, str_random(10)])
     ];
 });
